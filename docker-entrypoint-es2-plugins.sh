d15e633227a0404c878d3fce3d50c54dffd3f073
VishalMaral <vishalrmaral@gmail.com>
2017-02-18 18:08:52 +0000

    docker container

---
+++ docker-entrypoint-es2-plugins.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# setting up prerequisites
+
+# re-runs will give an error that is harmless:
+#   > ERROR: plugin directory /usr/share/elasticsearch/plugins/delete-by-query already exists.
+#   > To update the plugin, uninstall it first using 'remove delete-by-query' command
+#plugin install delete-by-query
+
+# https://github.com/mobz/elasticsearch-head/#running-as-a-plugin-of-elasticsearch
+plugin install mobz/elasticsearch-head
+
+# access it at /_plugin/elasticsearch-inquisitor/
+plugin install VishalMaral/elasticsearch-inquisitor
+
+#exec /docker-entrypoint.sh elasticsearch
+exec elasticsearch -Des.insecure.allow.root=true
